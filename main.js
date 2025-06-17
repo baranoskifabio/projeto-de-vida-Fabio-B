8835c2e716beba4e270664b60e47e7cca6fea3c0
baranoskifabio <145785098+baranoskifabio@users.noreply.github.com>
2025-06-17 07:53:28 -0300

    .

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(",abaa-conteudo")
 
-for (let i=0; i<botoes,length;++){
-botoes[i], onclick = function(){
-    botoes[i],onclick = function()}
-    for(let j=0; j< botoes. length; j++){
-botoes[j]. classList.remove("ativo");
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo")
+       }
+
+        botoes[i].classList.add("ativo");
+     botoes[i].classList.add("ativo");
 
     }
-    
-
-
-
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("10-01-2025T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0]. textContent = tempoObjetivo1- tempoAtual;
