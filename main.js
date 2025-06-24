8b77554b8a4f621c6b5a95917978ec4622820781
baranoskifabio <145785098+baranoskifabio@users.noreply.github.com>
2025-06-24 08:07:22 -0300

    ..

--- main.js
+++ main.js
@@ -20,3 +20,11 @@ const tempoObjetivo1 = new Date("10-01-2025T00:00:00");
 let tempoAtual = new Date();
 
 contadores[0]. textContent = tempoObjetivo1- tempoAtual;
+
+function calculaTempo (tempoObjetivo){
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundas = Math.floor(tempo/1000);
+let minutos = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
+}
