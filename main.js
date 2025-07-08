dcdc313fbea7a17184dc80a0b5620cc83b4c51d9
baranoskifabio <145785098+baranoskifabio@users.noreply.github.com>
2025-07-08 07:58:00 -0300

    ....

--- main.js
+++ main.js
@@ -33,19 +33,17 @@ function calculaTempo(tempoObjetivo){
     segundos %= 60;
     minutos %=60;
     horas %=24;
+
     if(tempoFinal>0){
-
-    
-
-    return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
+       return [dias, horas, minutos, segundos];
     }else{
-        return "Tempo Esgotado"
+        return [0,0,0,0];
     }
 }
 
 function atualizaCronometro(){
     for (let i=0; i<contadores.length; i++){
-        contadores[i].textContent=calculaTempo(tempos[i]);
+        document getElementById ("dias" + i). textContent = calculaTempo(tempos[i])[0]
     }
 }
 
